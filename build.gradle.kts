f5856a52169b10676ab9d1eb9a1dcb5bab0b6d4f
funzter <funzter@mail.timesnake.de>
2025-03-13 19:58:09 +0100

    :wrench: update dependency management

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,10 +64,10 @@ publishing {
 
     publications {
         create<MavenPublication>("maven") {
-            from(components["java"])
-        }
-        create<MavenPublication>("plugin") {
-            artifact(pluginArtifact)
+            artifacts {
+                from(components["java"])
+                artifact(pluginArtifact)
+            }
         }
     }
 }
